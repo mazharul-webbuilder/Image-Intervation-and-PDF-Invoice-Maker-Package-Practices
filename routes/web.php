ad8e5b81187b964563a07fdffc4957a55b20ee90
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-09-03 22:23:59 +0600

    design for invoice package test

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,24 @@ use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
+use App\Http\Controllers\LaravelInvoiceController;
+use App\Http\Controllers\DomPdfController;
 
 
-Route::get('/', function () {
-
-    return view('welcome');
+Route::get('/', function (Request $request){
+   return view('invoice');
 });
 
-Route::post('/', function (Request $request) {
-    dd($request->all());
-});
 
-//laravel live-wire package
-Route::get('/counter', Counter::class);
+//Route::get('/', function () {
+//
+//    return view('welcome');
+//});
+//
+//Route::post('/', function (Request $request) {
+//    dd($request->all());
+//});
+//
+////laravel live-wire package
+//Route::get('/counter', Counter::class);
 
